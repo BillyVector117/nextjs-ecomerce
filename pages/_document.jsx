0dba490db221b4ffdd6dcb64fe365a49a7141c0a
Billy Rodriguez <billyvecuss@hotmail.com>
2021-10-05 11:21:15 -0500

    Implementing a page for order-history and Profile

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -58,7 +58,8 @@ MyDocument.getInitialProps = async (ctx) => {
 
   ctx.renderPage = () =>
     originalRenderPage({
-      enhanceApp: (App) => (props) => {<App emotionCache={cache} {...props} />},
+      // eslint-disable-next-line react/display-name
+      enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
     });
 
   const initialProps = await Document.getInitialProps(ctx);
