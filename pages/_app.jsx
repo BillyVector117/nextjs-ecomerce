5ab4d0c1085b1aa27ab7a36f36bd44826c749c0d
Billy Rodriguez <billyvecuss@hotmail.com>
2021-10-05 10:38:47 -0500

    Implementing a page for order-history and Profile

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,12 @@
 
 import * as React from 'react';
-import { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
-/* import theme from '../src/theme'; */
 import createEmotionCache from '../src/createEmotionCache';
-import { useEffect } from 'react'
 import '../styles/globals.css'
-import { Store, StoreProvider } from '../context/Store'
-import { createTheme } from '@material-ui/core';
+import { StoreProvider } from '../context/Store'
 import Lay from '../components/Lay';
 import { SnackbarProvider } from 'notistack';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
@@ -20,36 +15,6 @@ import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
-  /*  const { state } = useContext(Store)
-   console.log(state)
-   const { darkMode } = state; // Change palette color depends on its value
-   const theme = createTheme({
-     typography: {
-       h1: {
-         fontSize: '1.6rem',
-         fontWeight: 400,
-         margin: '1rem 0'
-       },
-       h2: {
-         fontSize: '1.4rem',
-         fontWeight: 400,
-         margin: '1rem 0'
-       },
-       body1: {
-         fontWeight: 'normal'
-       },
- 
-     },
-     palette: {
-       mode: darkMode ? 'dark' : 'light',
-       primary: {
-         main: '#e00e0e'
-       },
-       secondary: {
-         main: '#fff'
-       }
-     }
-   }) */
   return (
     <CacheProvider value={emotionCache}>
       <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
@@ -60,18 +25,14 @@ function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) 
               <meta name="viewport" content="initial-scale=1, width=device-width" />
             </Head>
             <Lay>
-
               {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
               <CssBaseline />
               <Component {...pageProps} />
             </Lay>
           </PayPalScriptProvider>
-
         </StoreProvider>
-
       </SnackbarProvider>
     </CacheProvider>
-
   )
 }
 
